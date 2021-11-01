dd32a9c6cd65aff953866c5a8cc1287355314063
jhong92 <hjsky1010@gmail.com>
2021-11-02 00:07:36 +0900

    [네트워크] Chapter3 정리 진행중

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+주소 : [jhong92-pro.github.io](https://jhong92-pro.github.io/)
+
 # [Minimal Mistakes Jekyll theme](https://mmistakes.github.io/minimal-mistakes/)
 
 [![LICENSE](https://img.shields.io/badge/license-MIT-lightgrey.svg)](https://raw.githubusercontent.com/mmistakes/minimal-mistakes/master/LICENSE)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -14,6 +14,12 @@ var store = [{
         "title": "Chapter2 application layer",
         "excerpt":"Server Client Architecture 서버 always-on host IP 주소 고정 데이터 센터 존재 클라이언트 필요시에만 연결 동적 IP 주소 가질 수 있음 클라이언트끼리 통신하지 않음 Client-Server Computing의 장점 데이터를 서버에서만 제공하기 때문에 서버만 유저의 authorization과 데이터 protection을 관리하면 된다 기능의 분리(서버는 데이터를 주기만 하고 클라이언트는 데이터를 받기만 한다)로 모든 노드가 독립적이고...","categories": ["컴퓨터 네트워크"],
         "tags": ["복습"],
-        "url": "/%EC%BB%B4%ED%93%A8%ED%84%B0%20%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/%EC%BB%B4%EB%84%A4/",
+        "url": "/%EC%BB%B4%ED%93%A8%ED%84%B0%20%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/%EC%BB%B4%EB%84%A4_chapter2/",
+        "teaser": null
+      },{
+        "title": "Chapter3 Transport Layer",
+        "excerpt":"Transport vs. network layer Network layer : 호스트끼리 통신 Transport layer : 호스트의 프로세스끼리 통신 Multiplexing, Demultiplexing Multiplexings Multiplexing : 여러신호를 변환하여 하나의 매체에 담아 보냄 데이터 송신 시 ‘여러’ 소켓의 데이터가 header를 달고 ‘하나의’ transport layer로 보내짐 Demultiplexing Demultiplexing : 매체에 담긴 신호를 원래의 신호로 복원 데이터 수신 시...","categories": ["컴퓨터 네트워크"],
+        "tags": ["복습"],
+        "url": "/%EC%BB%B4%ED%93%A8%ED%84%B0%20%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/%EC%BB%B4%EB%84%A4_chapter3/",
         "teaser": null
       }]
